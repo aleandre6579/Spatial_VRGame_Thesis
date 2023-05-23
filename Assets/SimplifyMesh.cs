5363dc24da17c07d2a0dbf0ee6c81b383bcb83be
Alexandre Simon <aleandre6579.simon@gmail.com>
2023-05-23 15:50:35 +0700

    Initial commit

---
+++ Assets/SimplifyMesh.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Serialization;
+
+public class SimplifyMesh : MonoBehaviour
+{
+
+    public float quality = 0.5f;
+
+    void OnGUI()
+    {
+        if(GUI.Button(Rect.MinMaxRect(0, 0, 200, 200), "Simplify Mesh"))
+        {
+            var originalMesh = GetComponent<MeshFilter>().sharedMesh;
+            var meshSimplifier = new UnityMeshSimplifier.MeshSimplifier();
+            meshSimplifier.Initialize(originalMesh);
+            meshSimplifier.SimplifyMesh(quality);
+            var destMesh = meshSimplifier.ToMesh();
+            GetComponent<MeshFilter>().sharedMesh = destMesh;
+        }
+    }
+
+
+
+}
